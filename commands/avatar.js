fa30497cd226c10fcfb926e4ff69cd7ef5795f4e
Million900o <apaolini900o@gmail.com>
2020-12-30 04:28:28 -0500

    fix

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,6 +5,12 @@ module.exports = {
   name: 'avatar',
   aliases: ['av'],
   run: (client, message, args) => {
+    if(message.mentions) {
+      const user = client.users.get(message.mentions[0]);
+      message.channel.send(user.avatarUrl);
+      return;
+    }
     message.channel.send(message.user.avatarUrl);
+    return;
   }
 }
