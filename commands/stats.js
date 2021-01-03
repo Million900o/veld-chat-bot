2faf79e3a122abc93d20891e7a39e368754a4548
Million900o <apaolini900o@gmail.com>
2021-01-03 16:20:11 -0500

    better memory stuff

--- commands/stats.js
+++ commands/stats.js
@@ -18,7 +18,8 @@ class StatsCommand extends Command {
     const mem = {};
     Object.entries(process.memoryUsage()).map(e => mem[e[0]] = Math.round(e[1] / 1024 / 1024 * 10) / 10 + "MB");
     this.client.sendEmbed(message, 'Stats', [
-      { name: 'Memory', value: mem.rss },
+      { name: 'Reserved Memory', value: mem.rss },
+      { name: 'Used Memory', value: mem.heapUsed },
       { name: 'Cached Users', value: cachedUsers },
       { name: 'Cached Channels', value: cachedChannels }
     ]);
