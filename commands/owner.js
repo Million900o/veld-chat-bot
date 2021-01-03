982fe9c5f1f334c9d91e00aefc44e2c840b41cbc
Million900o <apaolini900o@gmail.com>
2021-01-03 16:41:32 -0500

    fixing owner command

--- commands/owner.js
+++ commands/owner.js
@@ -14,10 +14,9 @@ class OwoCommand extends Command {
   }
 
   run(message, args) {
-    return message.channel.send('This command will not work until message mentions are introduced.')
     if (!this.client.DB.collection('settings').get('owners').includes(message.author.id)) return;
     if (message.mentions) {
-      const user = this.client.users.get(message.mentions[0]);
+      const user = this.client.cache.users.get(message.mentions[0]);
       if (user) {
         const owners = this.client.DB.collection('settings').get('owners');
         if (owners.includes(user.id)) {
@@ -29,7 +28,21 @@ class OwoCommand extends Command {
           message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription(`${user.name} is now bot owner!`).setFooter('Developed By MILLION'));
           return;
         }
-      } else return message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription('User is not cached.').setFooter('Developed By MILLION'));
+      } else if (args.length) {
+        const user = Object.values(this.client.cache.users).find(u => u.name == args.join(' '));
+        if (user) {
+          const owners = this.client.DB.collection('settings').get('owners');
+          if (owners.includes(user.id)) {
+            owners.splice(owners.indexOf(user.id, 1));
+            message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription(`${user.name} is no longer bot owner.`).setFooter('Developed By MILLION'));
+            return;
+          } else {
+            this.client.DB.collection('settings').push('owners', user.id);
+            message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription(`${user.name} is now bot owner!`).setFooter('Developed By MILLION'));
+            return;
+          }
+        } else message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription('User not found.').setFooter('Developed By MILLION'));
+      } else return message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription('No mention or user name provided.').setFooter('Developed By MILLION'));
     } else return message.channel.send(new Embed().setAuthor(message.author.name, message.author.avatarURL).setDescription('You must include a user\'s name.').setFooter('Developed By MILLION'));
   }
 }
