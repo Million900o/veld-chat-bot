588ad3e1b0211d9d25e9f365d0680f173135bc61
Million900o <apaolini900o@gmail.com>
2021-01-05 07:31:56 -0500

    adding handling to the anime api

--- commands/hug.js
+++ commands/hug.js
@@ -17,11 +17,21 @@ class AvatarCommand extends Command {
   async run(message, args) {
     const user = this.client.cache.users.values().find(u => u.name == args.join(' ')) || message.author;
     const response = await shiro.sfw('hug');
-    const embed = new Embed()
-      .setAuthor(message.author.name + ' hugged ' + user.name, message.author.avatarURL)
-      .setThumbnail(response.url);
-    message.channel.send(embed);
-    return;
+    const code = response.code
+    if(code === 200) {
+      const embed = new Embed()
+        .setAuthor(message.author.name + ' hugged ' + user.name, message.author.avatarURL)
+        .setThumbnail(response.url);
+      message.channel.send(embed);
+      return;
+    } else {
+      const embed = new Embed()
+        .setAuthor(message.author.name + ' | Hug', message.author.avatarURL)
+        .setDescription('An unknown error has occured, try again later.')
+        .setFooter('Developed By MILLION');
+      message.channel.send(embed);
+      return;
+    }
   }
 }
 
